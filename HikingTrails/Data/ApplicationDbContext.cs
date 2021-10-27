a50715ab3204ce5a7a6302d6b07b22f5ebf09927
judson3 <judson3@g.clemson.edu>
2021-10-27 19:47:29 -0400

    Adds model, view, and controller for Trail

--- HikingTrails/Data/ApplicationDbContext.cs
+++ HikingTrails/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using HikingTrails.Models;
 
 namespace HikingTrails.Data
 {
@@ -12,5 +13,6 @@ namespace HikingTrails.Data
             : base(options)
         {
         }
+        public DbSet<HikingTrails.Models.Trail> Trail { get; set; }
     }
 }
---
+++ HikingTrails/Models/Trail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HikingTrails.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string trailName { get; set; }
+        public int rating { get; set; }
+        public string review { get; set; }
+
+        public Trail()
+        {
+
+        }
+
+    }
+}
