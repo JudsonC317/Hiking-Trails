7d053330b8389e9a3eb5196c7d78ec147f56a7f0
judson3 <84407887+JudsonC317@users.noreply.github.com>
2021-10-28 19:54:03 -0400

    Updates display names of variables

--- HikingTrails/Models/Trail.cs
+++ HikingTrails/Models/Trail.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HikingTrails.Models
 {
     public class Trail
     {
         public int Id { get; set; }
+
+        [Display(Name = "Trail Name")]
         public string trailName { get; set; }
+
+        [Display(Name = "Rating")]
         public int rating { get; set; }
+
+        [Display(Name = "Review")]
         public string review { get; set; }
 
         public Trail()
