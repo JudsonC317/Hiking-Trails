99588da1fcafc7839243269519ee506941ae4dba
judson3 <84407887+JudsonC317@users.noreply.github.com>
2021-10-27 20:28:40 -0400

    Adds seed data for database

---
+++ HikingTrails/Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using HikingTrails.Data;
+using System;
+using System.Linq;
+
+namespace HikingTrails.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Trail.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Trail.AddRange(
+                    new Trail
+                    {
+                        trailName = "Cat Rock",
+                        rating = 5,
+                        review = "Good view at the top, if you're able to make the steep climb."
+                    },
+
+                    new Trail
+                    {
+                        trailName = "Piney Run",
+                        rating = 4,
+                        review = "Cool lake and birdhouse."
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- HikingTrails/Program.cs
+++ HikingTrails/Program.cs
@@ -6,6 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HikingTrails.Data;
+using HikingTrails.Models;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace HikingTrails
 {
@@ -13,7 +17,25 @@ namespace HikingTrails
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
